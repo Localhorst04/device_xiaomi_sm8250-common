5c98a2053f17e48a147655d23bd4e797609b5944
wenchangliu <wenchangliu@google.com>
2020-06-20 18:47:09 +0800

    sm8250-common: video: enbale adaptive b-frame capability
    'enc_default_bframe_count' set to 1.
    'enc_max_mbs_per_sec' set to max-hw-load.
    
    Bug: 156724722
    Test: run ECO experiments
    Change-Id: I884be887e9ae71bb9f8778f8bd0f5dd728d1ba7e

--- kona.mk
+++ kona.mk
@@ -273,7 +273,8 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_kona.xml
     media_codecs_kona.xml \
     media_codecs_performance_kona.xml \
-    media_codecs_performance_c2.xml
+    media_codecs_performance_c2.xml \
+    video_system_specs.json
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
